cb0689d1e459a7021e3160ad0d4bbca2a1cde4b3
PegasusWithoutWings <georgezhangtj97@gmail.com>
2018-02-11 13:35:32 -0800

    Checkout the original vertion of timespec.h to create bug-free patch.

--- assign4/coreutils-with-bug/lib/timespec.h
+++ assign4/coreutils-with-bug/lib/timespec.h
@@ -45,12 +45,8 @@ struct timespec
 static inline int
 timespec_cmp (struct timespec a, struct timespec b)
 {
-  if (a.tv_sec < b.tv_sec):
-      return -1;
-  else if (a.tv_sec > b.tv_sec):
-      return 1;
-  else:
-      return (a.tv_nsec - b.tv_nsec);
+  int diff = a.tv_sec - b.tv_sec;
+  return diff ? diff : a.tv_nsec - b.tv_nsec;
 }
 
 # if ! HAVE_DECL_NANOSLEEP
