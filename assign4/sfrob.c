05a2f2d604cf258ac67d52d513b644e496edaa0e
WinglessPegasus <georgezhangtj97@gmail.com>
2018-02-14 00:12:28 -0500

    Finished the project.

--- assign4/sfrob.c
+++ assign4/sfrob.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 #include <errno.h>
 
-int frobcmp (const char *str1, const char *str2) {
-  register const unsigned char *s1 = (const unsigned char*)str1;
-  register const unsigned char *s2 = (const unsigned char*)str2;
-
-  for (; *s1 == *s2; s1++, s2++) {
+int frobcmp (char const* s1, char const* s2) {
+  for ( ; *s1 == *s2; s1++, s2++) {
     if (*s1 == ' '){
       return 0;
     }
-    return ((*s1 ^ 42) < (*s2 ^ 42)) ? -1 : 1;
   }
+  return ((*s1 ^ 42) < (*s2 ^ 42)) ? -1 : 1;
 }
 
 // This is a wrapper function to use frobcmp in qsort
