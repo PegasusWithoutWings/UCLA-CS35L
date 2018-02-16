78eb210cee0e537f1b0fcd42ce020e8603d56f7b
WinglessPegasus <georgezhangtj97@gmail.com>
2018-02-15 20:32:43 -0500

    Initialize files.

---
+++ assign5/tr2b.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+/* 
+Note: By all means I intend to use a hashtable for this program. Sadly, it
+is not available in standard C libraries. So, I am just going to use an 
+inefficient algorithm here rather than implementing a hashtable from scratch.
+*/
+
