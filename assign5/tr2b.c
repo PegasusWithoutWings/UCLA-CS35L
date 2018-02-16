f03691cd9669b78a286219aa4f5818726f04b73a
WinglessPegasus <georgezhangtj97@gmail.com>
2018-02-15 21:08:27 -0500

    Play around with the hashtable example.

--- assign5/tr2b.c
+++ assign5/tr2b.c
@@ -9,10 +9,35 @@ char *data[] = { "alpha", "bravo", "charlie", "delta",
      "victor", "whisky", "x-ray", "yankee", "zulu"
 };
 
-// As a first step, we will get comfortable with getchar and stdin
-int main() {
-  while (!feof(stdin)) {
-    fputc(fgetc(stdin), stdout);
-  }
-  return 0;
+int
+main(void)
+{
+    ENTRY e, *ep;
+    int i;
+
+   hcreate(30);
+
+   for (i = 0; i < 24; i++) {
+        e.key = data[i];
+        /* data is just an integer, instead of a
+           pointer to something */
+        e.data = (void *) i;
+        ep = hsearch(e, ENTER);
+        /* there should be no failures */
+        if (ep == NULL) {
+            fprintf(stderr, "entry failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+   for (i = 22; i < 26; i++) {
+        /* print two entries from the table, and
+           show that two are not in the table */
+        e.key = data[i];
+        ep = hsearch(e, FIND);
+        printf("%9.9s -> %9.9s:%d\n", e.key,
+               ep ? ep->key : "NULL", ep ? (int)(ep->data) : 0);
+    }
+    hdestroy();
+    exit(EXIT_SUCCESS);
 }
