b853e4c4bb8486fdb7acf623ac911da371f17fbc
WinglessPegasus <georgezhangtj97@gmail.com>
2018-03-07 15:31:45 -0800

    Finish the implementation of randlibsw.c

--- assign8/dlskeleton/randall.c
+++ assign8/dlskeleton/randall.c
@@ -29,63 +29,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Hardware implementation.  */
-
-/* Initialize the hardware rand64 implementation.  */
-static void
-hardware_rand64_init (void)
-{
-}
-
-/* Return a random value, using hardware operations.  */
-static unsigned long long
-hardware_rand64 (void)
-{
-  unsigned long long int x;
-  while (! _rdrand64_step (&x))
-    continue;
-  return x;
-}
-
-/* Finalize the hardware rand64 implementation.  */
-static void
-hardware_rand64_fini (void)
-{
-}
-
-
-
-/* Software implementation.  */
-
-/* Input stream containing random bytes.  */
-static FILE *urandstream;
-
-/* Initialize the software rand64 implementation.  */
-static void
-software_rand64_init (void)
-{
-  urandstream = fopen ("/dev/urandom", "r");
-  if (! urandstream)
-    abort ();
-}
-
-/* Return a random value, using software operations.  */
-static unsigned long long
-software_rand64 (void)
-{
-  unsigned long long int x;
-  if (fread (&x, sizeof x, 1, urandstream) != 1)
-    abort ();
-  return x;
-}
-
-/* Finalize the software rand64 implementation.  */
-static void
-software_rand64_fini (void)
-{
-  fclose (urandstream);
-}
-
 static bool
 writebytes (unsigned long long x, int nbytes)
 {
