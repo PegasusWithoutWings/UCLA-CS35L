cca349b676a6ef0783d51a3ec49cebfb84d63f97
WinglessPegasus <georgezhangtj97@gmail.com>
2018-03-07 19:42:21 -0800

    Fixed some dependency bugs in randlibhw and randlibsw.

--- assign8/dlskeleton/randmain.mk
+++ assign8/dlskeleton/randmain.mk
@@ -6,8 +6,8 @@ CFLAGS = $(OPTIMIZE) -g3 -Wall -Wextra -march=native -mtune=native -mrdrnd
 randmain: randmain.c randcpuid.c
 	$(CC) $(CFLAGS) randmain.c randcpuid.c -ldl -o randmain 
 
-randlibsw.so: randlibsw.c randlibsw.h
+randlibsw.so: randlibsw.c
 	$(CC) $(CFLAGS) -shared -fPIC randlibsw.c -o randlibsw.so
 
-randlibhw.so: randlibhw.c randlibhw.h
+randlibhw.so: randlibhw.c
 	$(CC) $(CFLAGS) -shared -fPIC randlibhw.c -o randlibhw.so
