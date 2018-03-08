c530d10fb84513b848cab4d044063c0e8bff6a7c
WinglessPegasus <georgezhangtj97@gmail.com>
2018-03-07 20:09:24 -0800

    Added rpath option to the reciple for making randmain. It now correctly works in SEASnet server.

--- assign8/dlskeleton/randmain.mk
+++ assign8/dlskeleton/randmain.mk
@@ -3,8 +3,8 @@ OPTIMIZE = -O2
 CC = gcc
 CFLAGS = $(OPTIMIZE) -g3 -Wall -Wextra -march=native -mtune=native -mrdrnd
 
-randmain: randmain.c randcpuid.c
-	$(CC) $(CFLAGS) randmain.c randcpuid.c -ldl -o randmain 
+randmain: randmain.c randcpuid.c randlibsw.so randlibhw.so
+	$(CC) $(CFLAGS) -Wl,-rpath=$(PWD) randmain.c randcpuid.c -ldl -o randmain 
 
 randlibsw.so: randlibsw.c
 	$(CC) $(CFLAGS) -shared -fPIC randlibsw.c -o randlibsw.so
